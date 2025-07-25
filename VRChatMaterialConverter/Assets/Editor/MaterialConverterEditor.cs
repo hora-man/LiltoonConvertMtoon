d3917b18adbf40bd7aeaa423493153ffd7e6d1ed
hora-man <horayuki737@gmail.com>
2025-07-25 13:57:34 +0900

    Update MaterialConverterEditor.cs

--- VRChatMaterialConverter/Assets/Editor/MaterialConverterEditor.cs
+++ VRChatMaterialConverter/Assets/Editor/MaterialConverterEditor.cs
@@ -16,3 +16,38 @@ public class MaterialConverterEditor : Editor
         }
     }
 }
+
+// --- 追加: Toolsメニューから起動できるウィンドウ ---
+public class MaterialConverterWindow : EditorWindow
+{
+    private MaterialConverter converter;
+
+    [MenuItem("Tools/Material Converter")]
+    public static void ShowWindow()
+    {
+        GetWindow<MaterialConverterWindow>("Material Converter");
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("Material Converter", EditorStyles.boldLabel);
+
+        converter = (MaterialConverter)EditorGUILayout.ObjectField(
+            "Material Converter",
+            converter,
+            typeof(MaterialConverter),
+            true);
+
+        if (converter != null)
+        {
+            if (GUILayout.Button("Convert Liltoon to Mtoon"))
+            {
+                converter.ConvertMaterials();
+            }
+        }
+        else
+        {
+            EditorGUILayout.HelpBox("MaterialConverterをシーン内から選択してください。", MessageType.Info);
+        }
+    }
+}
